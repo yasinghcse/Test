0d3551189b48924a567344535c005a40d5f18af2
Yadwinder <yadwinder.singh11@gmail.com>
2016-07-31 15:09:22 +0530

    full bootstrap designing

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ActiveRecord::Base
-  validates :title, presence: true, length: { minimum: 5, maximum: 10 }
-  validates :description, presence:true, length: { minimum: 10, maximum: 50 }
+  validates :title, presence: true, length: { minimum: 5, maximum: 50 }
+  validates :description, presence:true, length: { minimum: 10, maximum: 1000 }
 end
