6d3506360345f5a1e52e5fb30c1bf2b82a691a20
Yadwinder <yadwinder.singh11@gmail.com>
2016-08-02 00:21:52 +0530

    validation on user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
 class User < ActiveRecord::Base
+  # create user unique and mandatory
+  # create email unique and mandatory
+  # validate email using regex
+
+  #uniqueness: true -- this option will not work for case senstive data
+  validates :username, presence: true, 
+            uniqueness: { case_sensitive: false },
+            length: {minimum:3, maximum: 25}
+
+  #test regular experssion at rebular.com
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i          
+  validates :email, presence: true,
+            uniqueness: { case_sensitive: false},
+            length: {maximum: 105},
+            format: { with: VALID_EMAIL_REGEX }
 end
