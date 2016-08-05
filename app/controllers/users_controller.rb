ae1058ae695194e7445defece377f585dd300ce7
Yadwinder <yadwinder.singh11@gmail.com>
2016-08-06 00:50:43 +0530

    user edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,9 +13,23 @@ def create
   end
 end
 
+def edit
+  @user = User.find(params[:id])
+end
+
+def update
+  @user = User.find(params[:id])
+  if @user.update(user_params)
+    flash[:success] = "User Updated successfully"
+    redirect_to articles_path
+  else
+    render 'edit'
+  end
+end
+
 def user_params
   params.require(:user).permit(:username, :email, :password)
 end
 
-end
+end 
  
