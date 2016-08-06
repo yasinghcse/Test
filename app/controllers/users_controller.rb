4f94023c2267ac37771c0b23aee8a3bcb50db717
Yadwinder <yadwinder.singh11@gmail.com>
2016-08-06 16:08:07 +0530

    Authenticatiosn' '

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+before_action :set_user, only: [:edit, :show, :update]
+before_action :require_same_user, only: [:edit, :update]
 
 def index
   @users = User.paginate(page: params[:page], per_page: 5)
@@ -19,11 +21,9 @@ def create
 end
 
 def edit
-  @user = User.find(params[:id])
 end
 
 def update
-  @user = User.find(params[:id])
   if @user.update(user_params)
     flash[:success] = "User Updated successfully"
     redirect_to articles_path
@@ -32,13 +32,24 @@ def update
   end
 end
 
-def show
-  @user = User.find(params[:id])
+def show  
   @user_articles = @user.articles.paginate(page: params[:page], per_page: 3)
 end
 
+private
 def user_params
   params.require(:user).permit(:username, :email, :password)
+end
+
+def set_user
+  @user = User.find(params[:id])
+end
+
+def require_same_user
+  if current_user != @user
+    flash[:danger] = "You can only edit or update you own accounts"
+    redirect_to root_path
+  end
 end
 
 end 
