658b98f9b5b7a7deb9dc8b91986cf833dd41f857
Yadwinder <yadwinder.singh11@gmail.com>
2016-08-18 00:08:03 +0530

    test

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -22,6 +22,7 @@ end
 
 def show
    @category=Category.find(params[:id]) 
+   @category_articles = @category.articles.paginate(page: params[:page], per_page:5 )
 end
 
 
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ActiveRecord::Base
+  has_many :article_categories
+  has_many :articles, through: :article_categories
   validates :name, presence: true, uniqueness: { case_sensitive: false },
             length: {minimum:3, maximum: 25}
   # or for uniq , below code can be used
