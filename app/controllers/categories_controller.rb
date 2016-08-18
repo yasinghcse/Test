3355b33512269eca0d2c0c3c239cb30ca6a25670
Yadwinder <yadwinder.singh11@gmail.com>
2016-08-18 19:56:54 +0530

    catgeories pages

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -19,10 +19,24 @@ def create
   end
 end
 
-
 def show
    @category=Category.find(params[:id]) 
    @category_articles = @category.articles.paginate(page: params[:page], per_page:5 )
+end
+
+def edit
+  @category=Category.find(params[:id]) 
+end
+
+def update
+  
+  @category=Category.find(params[:id]) 
+  if @category.update(category_params)
+    flash[:success] = "Category was updated successfully"
+    redirect_to categories_path(@category)
+  else
+    render 'edit'
+  end
 end
 
 
